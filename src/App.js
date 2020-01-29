62b0241568d6f87ec1cb809957d5a976f649c382
Liviu Preda <liviupreda@gmail.com>
2020-01-29 07:16:34 +0200

    add Login Page component; link to it by clicking on Sign Out button

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { useState } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import LoginPage from "./components/LoginPage";
 import "./App.css";
 
 function App() {
   return (
     <Router>
       <Switch>
+        <Route exact path="/" render={LoginPage} />
         <Route
           exact
           path="/home"
---
+++ src/components/LoginPage.js
@@ -0,0 +1,14 @@
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import { ValidatorForm, TextValidator } from "react-material-ui-form-validator";
+import { makeStyles } from "@material-ui/core/styles";
+
+function LoginPage(props) {
+  return (
+    <div>
+      <p>Login Page</p>
+    </div>
+  );
+}
+
+export default LoginPage;
