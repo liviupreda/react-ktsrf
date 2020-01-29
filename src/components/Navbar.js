62172adb32871ebf12f16dfbe7e45486def8d688
Liviu Preda <liviupreda@gmail.com>
2020-01-29 04:52:52 +0200

    change file structure in components folder

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,7 @@ import React from "react";
 const Navbar = () => {
   return (
     <nav style={{ display: "flex", justifyContent: "flex-end" }}>
-      <p>Logout</p>
+      <p className="f3 link dim black underline pa3 pointer">Logout</p>
     </nav>
   );
 };
