163df908720f3abb4e213bc2a0722144d4e4499f
Liviu Preda <liviupreda@gmail.com>
2020-01-29 06:27:23 +0200

    install axios for http requests and react-google-maps

--- src/styles/NavbarStyles.js
+++ src/styles/NavbarStyles.js
@@ -1,8 +1,36 @@
 export default {
   navbar: {
     display: "flex",
-    justifyContent: "flex-center",
+    justifyContent: "flex-start",
     alignItems: "center",
     height: "5vh"
+  },
+  logo: {
+    marginRight: "15px",
+    padding: "0 13px",
+    fontSize: "22px",
+    backgroundColor: "#eee",
+    fontFamily: '"Roboto", sans-serif',
+    height: "100%",
+    display: "flex",
+    alignItems: "center",
+    "& a": {
+      textDecoration: "none",
+      color: "black"
+    }
+  },
+  signout: {
+    marginRight: "15px",
+    padding: "0 13px",
+    fontSize: "22px",
+    backgroundColor: "#eee",
+    fontFamily: '"Roboto", sans-serif',
+    height: "100%",
+    display: "flex",
+    alignItems: "center",
+    "& a": {
+      textDecoration: "none",
+      color: "black"
+    }
   }
 };
