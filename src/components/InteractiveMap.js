2830de1fd88f84c27881393306399dcc45a0891b
Liviu Preda <liviupreda@gmail.com>
2020-01-29 11:40:32 +0200

    add InteractiveMap to UserHome

---
+++ src/components/InteractiveMap.js
@@ -0,0 +1,9 @@
+import React, { useState } from "react";
+
+export default function InteractiveMap(props) {
+  return (
+    <div>
+      <p>Interactive Google Maps</p>
+    </div>
+  );
+}
--- src/components/UserHome.js
+++ src/components/UserHome.js
@@ -3,11 +3,13 @@ import Button from "@material-ui/core/Button";
 import Navbar from "./Navbar";
 import SpotSearchForm from "./SpotSearchForm";
 import SpotsList from "./SpotsList";
+import InteractiveMap from "./InteractiveMap";
 
 function UserHome(props) {
   return (
     <div>
       <Navbar />
+      <InteractiveMap />
       <SpotSearchForm />
       <SpotsList />
     </div>
