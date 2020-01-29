3404423002bfd4876a3909a3ba74ecd20401fc51
Liviu Preda <liviupreda@gmail.com>
2020-01-29 12:16:54 +0200

    install google maps react

--- src/components/InteractiveMap.js
+++ src/components/InteractiveMap.js
@@ -1,9 +1,17 @@
-import React, { useState } from "react";
+import React from "react";
+import Button from "@material-ui/core/Button";
+import { withStyles } from "@material-ui/core/styles";
+import styles from "../styles/InteractiveMapStyles";
 
-export default function InteractiveMap(props) {
+function InteractiveMap(props) {
+  const API_KEY = "AIzaSyAFR9ohystEpVf1UyEPad9Mfh56S3-R6Qo";
+  const API_URL = `https://maps.googleapis.com/maps/api/js?key={${API_KEY}}&callback=initMap`;
+
   return (
-    <div>
+    <div className="map">
       <p>Interactive Google Maps</p>
     </div>
   );
 }
+
+export default withStyles(styles)(InteractiveMap);
