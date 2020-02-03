7161e95005836441799da71b6a5c540db7dbd9aa
Liviu Preda <liviupreda@gmail.com>
2020-02-03 16:25:45 +0200

    register new user and post to api /user

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,11 +1,45 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 function SignUp(props) {
-  const { setRoute, setIsLoggedIn } = props;
+  const { setRoute, setIsLoggedIn, apiUrl } = props;
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  async function createNewUser() {
+    const params = {
+      name: name,
+      email: email,
+      password: password
+    };
+
+    let res = await axios.post(apiUrl + "/user", params);
+    console.log(res.data);
+  }
+
+  const handleNameFieldChange = e => {
+    setName(e.target.value);
+  };
+
+  const handleEmailFieldChange = e => {
+    setEmail(e.target.value);
+  };
+
+  const handlePasswordFieldChange = e => {
+    setPassword(e.target.value);
+  };
+
+  const handleSubmit = () => {
+    createNewUser();
+    setRoute("home");
+    setIsLoggedIn(true);
+  };
+
   return (
     <article className="br3 ba b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
       <main className="pa4 black-80">
-        <div className="measure">
+        <form className="measure" onSubmit={handleSubmit}>
           <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
             <legend className="f2 fw6 ph0 mh0">Sign Up</legend>
             <div className="mt3">
@@ -17,6 +51,7 @@ function SignUp(props) {
                 type="text"
                 name="name"
                 id="name"
+                onChange={handleNameFieldChange}
               />
             </div>
             <div className="mt3">
@@ -28,6 +63,7 @@ function SignUp(props) {
                 type="email"
                 name="email-address"
                 id="email-address"
+                onChange={handleEmailFieldChange}
               />
             </div>
             <div className="mv3">
@@ -39,21 +75,18 @@ function SignUp(props) {
                 type="password"
                 name="password"
                 id="password"
+                onChange={handlePasswordFieldChange}
               />
             </div>
           </fieldset>
           <div className="">
             <input
-              onClick={() => {
-                setRoute("home");
-                setIsLoggedIn(true);
-              }}
               className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib"
               type="submit"
               value="Register"
             />
           </div>
-        </div>
+        </form>
       </main>
     </article>
   );
