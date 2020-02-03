3baa200273db0bfbe600d8705b90953534f911a7
Liviu Preda <liviupreda@gmail.com>
2020-02-03 14:21:42 +0200

    refactor PageFooter.js to remove a CRA warning

--- src/components/PageFooter.js
+++ src/components/PageFooter.js
@@ -4,13 +4,8 @@ import { withStyles } from "@material-ui/core/styles";
 import styles from "../styles/PageFooterStyles";
 
 function PageFooter(props) {
-  const { paletteName, emoji, classes } = props;
-  return (
-    <footer className={classes.footer}>
-      Liviu Preda 2020
-      {/* <span className={classes.footerEmoji}>{emoji}</span> */}
-    </footer>
-  );
+  const { classes } = props;
+  return <footer className={classes.footer}>Liviu Preda 2020</footer>;
 }
 
 export default withStyles(styles)(PageFooter);
