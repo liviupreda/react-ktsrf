620637e4e8ecf84731c3a05d3104073850116f0d
Liviu Preda <liviupreda@gmail.com>
2020-02-03 11:31:52 +0200

    finish login and signup frontend

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -5,7 +5,7 @@ const SignUp = props => {
   return (
     <article className="br3 ba b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
       <main className="pa4 black-80">
-        <form className="measure">
+        <div className="measure">
           <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
             <legend className="f2 fw6 ph0 mh0">Sign Up</legend>
             <div className="mt3">
@@ -53,7 +53,7 @@ const SignUp = props => {
               value="Register"
             />
           </div>
-        </form>
+        </div>
       </main>
     </article>
   );
